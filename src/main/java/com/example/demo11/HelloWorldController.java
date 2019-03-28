ee95a84e86516b1c158c6727210fe556fbba05e6
icelove82 <yunmyeonghun@gmail.com>
2019-03-28 18:35:18 +0900

    update3.

--- src/main/java/com/example/demo11/HelloWorldController.java
+++ src/main/java/com/example/demo11/HelloWorldController.java
@@ -7,10 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.slf4j.Slf4j;
-
 @RestController
-@Slf4j
 public class HelloWorldController {
 
     @Autowired
